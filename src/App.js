e7f098dc6762a46cf9d0888d95bba9c57744874b
Sanelly43 <133259966+Sanelly43@users.noreply.github.com>
2023-12-01 21:45:55 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Durban" />
+        <Weather defaultCity="Johannesburg" />
       </div>
     </div>
   );
